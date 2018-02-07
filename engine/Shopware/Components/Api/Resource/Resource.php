6f90a046f06113b786ee1aa6c978fad544e47219
Patrick Dornbusch <pd@netzdirektion.de>
2018-02-07 11:21:53 +0100

    SW-21098 Added missing type hint for setContainer method to prevent exception on cache clear

--- engine/Shopware/Components/Api/Resource/Resource.php
+++ engine/Shopware/Components/Api/Resource/Resource.php
@@ -100,7 +100,7 @@ abstract class Resource
     /**
      * @param $container
      */
-    public function setContainer($container)
+    public function setContainer(Container $container)
     {
         $this->container = $container;
     }
